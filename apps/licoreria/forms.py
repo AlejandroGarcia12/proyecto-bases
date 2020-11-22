f8e22d9fb9a33dc914cb0631f9c0d8ee8c8911b0
Alejandra-p <56894607+Alejandra-p@users.noreply.github.com>
2020-11-22 02:04:30 -0500

    Consultar, agregar, editar producto.

--- apps/licoreria/forms.py
+++ apps/licoreria/forms.py
@@ -1,36 +1,67 @@
 from django import forms
-from apps.licoreria.models import Cliente, Direccion, Producto, Carrito, Domiciliario, Factura
+from apps.licoreria.models import Cliente, Producto
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields =[
-        'id_cliente', 
-        'nombre',
-        'apellido',
-        'cedula',
-        'fecha_nacimiento',
-        'telefono', 
-        'correo',
-        'clave',
+        fields = [
+            'id_cliente',
+            'nombre',
+            'apellido',
+            'cedula',
+            'fecha_nacimiento',
+            'telefono',
+            'correo',
+            'clave',
         ]
-        fields ={
-        'id_cliente':'Id', 
-        'nombre':'Nombre',
-        'apellido':'Apellido',
-        'cedula':'Cedula',
-        'fecha_nacimiento':'Fecha de nacimiento',
-        'telefono':'Telefono', 
-        'correo':'Correo',
-        'clave':'Clave',
+        fields = {
+            'id_cliente' : 'Id',
+            'nombre' : 'Nombre',
+            'apellido' : 'Apellido',
+            'cedula' : 'Cédula',
+            'fecha_nacimiento' : 'Fecha',
+            'telefono' : 'Teléfono',
+            'correo' : 'Correo',
+            'clave' : 'Clave',
         }
-        widgets={
-        'id_cliente':forms.TextInput(), 
-        'nombre':forms.TextInput(),
-        'apellido':forms.TextInput(),
-        'cedula':forms.NumberInput(),
-        'fecha_nacimiento':forms.TextInput(),
-        'telefono':forms.NumberInput(), 
-        'correo':forms.TextInput(),
-        'clave':forms.TextInput(),
+        widgets = {
+            'id_cliente' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'nombre' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'apellido' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'cedula' : forms.NumberInput(attrs = {'class' : 'form-control'}),
+            'fecha_nacimiento' : forms.DateInput(attrs = {'class' : 'form-control'}),
+            'telefono' : forms.NumberInput(attrs = {'class' : 'form-control'}),
+            'correo' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'clave' : forms.TextInput(attrs = {'class' : 'form-control'}),
+        }
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = [
+            'id_producto',
+            'nombre',
+            'precio',
+            'tamano',
+            'unidades_disponibles',
+            'id_admin',
+            'id_categoria',
+        ]
+        fields = {
+            'id_producto' : 'Id',
+            'nombre' : 'Nombre',
+            'precio' : 'Precio',
+            'tamano' : 'Tamaño ml',
+            'unidades_disponibles' : 'Unidades',
+            'id_admin' : 'Id administrador',
+            'id_categoria' : 'Id categoria',
+        }
+        widgets = {
+            'id_producto' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'nombre' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'precio' : forms.NumberInput(attrs = {'class' : 'form-control'}),
+            'tamano' : forms.NumberInput(attrs = {'class' : 'form-control'}),
+            'unidades_disponibles' : forms.NumberInput(attrs = {'class' : 'form-control'}),
+            'id_admin' : forms.NumberInput(attrs = {'class' : 'form-control'}),
+            'id_categoria' : forms.NumberInput(attrs = {'class' : 'form-control'}),
         }
--- licoreria/settings.py
+++ licoreria/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'licoreria',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'basesdedatos',
         'HOST': 'localhost',
         'PORT': 5432,
     }
